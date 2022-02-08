9f16322ff5a212de238d0f2605cfc4989af91d29
Tom Springbett <tspringbett@gmail.com>
2022-02-08 21:18:44 +0000

    The display now shows the button the user has clicked on

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    const display = document.getElementById('display')
+    display.innerHTML = calculator.displayValue;
+}
+
+
+const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            calculator.displayValue = button.id;
+            //display.innerHTML = calculator.displayValue;
+            
+            
+        })
+    });
+ 
+
+
+
 function add(a, b) {
  console.log(a + b);
-}
+};
+
+function subtract(a, b) {
+    console.log(a - b);
+};
+
+function multiply(a, b) {
+    console.log(a * b);
+};
+
+function divide(a, b) {
+    console.log(a / b);
+};
+
+function operate(a, b, c) {
+    if(a === "+") {
+        add(b, c)
+    } else if (a === "-") {
+        subtract(b, c)
+    } else if (a === "x") {
+        multiply(b, c) 
+    } else if (a === "÷") {
+        divide(b, c)
+    }
+};
